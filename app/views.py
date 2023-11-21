6fac46728b3dc5e35d0a1e443de379475f6dde08
maheswarigosala <maheswari18082002@gmail.com>
2023-11-21 11:24:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mahi(request):
+    return render(request,'mahi.html')
+def kiran(request):
+    return render(request,'kiran.html')
+def raji(request):
+    return render(request,'raji.html')
+
+
